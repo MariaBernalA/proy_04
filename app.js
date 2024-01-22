99920135d861582fe35ae92d3cdbc016cfe450ea
mknjbhvh <camila1034_@hotmail.com>
2024-01-21 23:59:35 -0500

    Se adjuntan comentarios en el codigo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require("colors");
+require("colors");//Se importa el módulo colors para agregar color
 
-const math = require('./modules/math.js')
+const math = require('./modules/math.js')//Se importa el módulo math que contiene las funciones matemáticas
 
-console.clear();
-console.log(math);
+console.clear();//Se utiliza console.clear() para limpiar la consola 
+console.log(math);//se imprime el objeto para ayudar a verificar que la importación y asignación de math se haya realizado correctamente.
 
-const main = async() => {
-    let f= (math.getRandomInt(10))
+const main = async() => {//unción llamada main que utiliza async para admitir operaciones asíncronas 
+    let f= (math.getRandomInt(10))//Se generan dos números aleatorios utilizando la función getRandomInt del módulo math. Estos números se almacenan en las variables f y m
     let m= (math.getRandomInt(10))
     console.log(':::::::::::::::::::::::::::::::::::::::::::::::::'.bgCyan);
     console.log('::'.bgCyan + 'Funciones matematicas'.bgBlue + ':::'.bgCyan);
@@ -17,7 +17,7 @@ const main = async() => {
 
     console.log('::::::::::::::::::::::::::::::::::::::::::::::::::.'.bgBlue);
     console.log(':::'.bgBlue + '       '+':::'.bgBlue);
-    console.log(':::'.bgBlue + 'Suma =>' + math.add(f,m) +'     '+ ':::'.bgBlue);
+    console.log(':::'.bgBlue + 'Suma =>' + math.add(f,m) +'     '+ ':::'.bgBlue);//Se imprime las operaciones matematicas
     console.log(':::'.bgBlue + 'Resta =>' + math.substract(f,m) +'    '+ ':::'.bgBlue);
     console.log(':::'.bgBlue + 'Multiplicacion =>' + math.multiply(f,m) +'    '+ ':::'.bgBlue);
     console.log(':::'.bgBlue + 'Division =>' + math.divide(f,m) +'    '+ ':::'.bgBlue);
@@ -25,4 +25,4 @@ const main = async() => {
 
 }
 
-main();
+main();//se cierra main para ejecutar la funcion en ella
--- modules/math.js
+++ modules/math.js
@@ -1,26 +1,26 @@
-const Maths = {}
+const Maths = {}//variable vacia que  se utiliza para agrupar las funciones relacionadas con operaciones matemáticas.
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomInt(max) {//función llamada getRandomInt que toma un parámetro max y devuelve un número entero aleatorio entre 0 
+    return Math.floor(Math.random() * max);//funciones que se utilzan para realizar la operacion Math.random() y Math.floor().
 }
 
-function add(a,b) {
+function add(a,b) {//función llamada add que toma dos parámetros para sumar
     return a + b;
 }
 
-function substract(a,b) {
+function substract(a,b) {//función llamada substract que toma dos parámetros para restar
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a,b) {//función llamada multiply que toma dos parámetros para multiplicar
     return a * b;
 }
 
-function divide(a,b){
-    if (b==0 && a ==0){
-        return 'Error: N/A :: valores a = ${a} y b = ${b}'
+function divide(a,b){//función llamada divide que toma dos parámetros para dividir
+    if (b==0 && a ==0){//incluye una verificación para evitar la división por cero
+        return 'Error: N/A :: valores a = ${a} y b = ${b}'// Si tanto a como b son cero, devuelve un mensaje de error.
     } 
-    else if (b == 0) {
+    else if (b == 0) {// Si solo b es cero, devuelve 'N/A'.
         return 'N/A'
     }
     else {
@@ -28,11 +28,11 @@ function divide(a,b){
     }
 }
 
-Maths.add = add;
+Maths.add = add;//estas funciones se asignan como propiedades al objeto Math.
 Maths.substract = substract;
 Maths.multiply = multiply;
 Maths.divide = divide;
 
 Maths.getRandomInt = getRandomInt
 
-module.exports = Maths;
+module.exports = Maths;//Se exporta Maths para importarla en el archivo app.js
